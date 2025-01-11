10cb9192d0468b84dbcc1266fe03efe5b2d5243e
202110861 <dbsalsk2514@naver.com>
2025-01-11 23:41:58 +0900

    chore: 홈 화면 디자인 코드 수정

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -2,10 +2,12 @@ import styled from "styled-components";
 import Banner from "../component/Banner";
 import Ranking from "../component/Ranking";
 import Dream from "../component/Dream";
+import Logo from "../component/Logo";
 
 export const HomePage = () => {
   return (
     <Container>
+      <Logo />
       <Banner />
       <Ranking />
       <Dream />
@@ -18,4 +20,6 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 30px;
+  margin-bottom: 23px;
 `;
