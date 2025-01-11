736e84b47f6b38228da599de83e56ad70607eaf0
김예린 <lynn1833@ewhain.net>
2025-01-12 08:25:56 +0900

    fix

--- src/app/styles/fonts.ts
+++ src/app/styles/fonts.ts
@@ -1,12 +1,4 @@
-import { FontsTypes } from '@shared/types';
-
-const createFontStyle = (family: string, weight: number, size: number, lineHeight: number) => `
-  font-family: "${family}";
-  font-weight: ${weight};
-  font-size: ${size}px;
-  line-height: ${lineHeight}px;
-  letter-spacing: 0%;
-`;
+import { FontsTypes } from "@shared/types";
 
 export const fonts: FontsTypes = {
   /*heading_bold_24px: createFontStyle('Pretendard', 700, 24, 34),
