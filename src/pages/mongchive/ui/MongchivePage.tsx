8d720dbf2b42083f321f79828b6df204ead292b9
김예린 <lynn1833@ewhain.net>
2025-01-12 00:04:25 +0900

    feat: 카테고리 버튼 수정

--- src/pages/mongchive/ui/MongchivePage.tsx
+++ src/pages/mongchive/ui/MongchivePage.tsx
@@ -1,21 +1,36 @@
 import styled from "styled-components";
-import ghost from "../../../shared/assets/image/ghost.png";
-import dog from "../../../shared/assets/image/dog.png";
-import clover from "../../../shared/assets/image/clover.png";
+import ghost from "../../../shared/assets/images/ghost.png";
+import dog from "../../../shared/assets/images/dog.png";
+import clover from "../../../shared/assets/images/clover.png";
+import { useState } from "react";
 
 export const MongchivePage = () => {
+  const [selected, setSelected] = useState<string | null>(null);
+
+  const handleClick = (name: string) => {
+    setSelected(name);
+  };
   return (
     <Container>
       <Options>
-        <Option>
+        <Option
+          isSelected={selected === "ghost"}
+          onClick={() => handleClick("ghost")}
+        >
           <img src={ghost} />
           <Name>공포</Name>
         </Option>
-        <Option>
+        <Option
+          isSelected={selected === "dog"}
+          onClick={() => handleClick("dog")}
+        >
           <img src={dog} />
           <Name>개꿈</Name>
         </Option>
-        <Option>
+        <Option
+          isSelected={selected === "clover"}
+          onClick={() => handleClick("clover")}
+        >
           <img src={clover} />
           <Name>일상</Name>
         </Option>
@@ -34,18 +49,19 @@ const Name = styled.div`
   line-height: 24px;
 `;
 
-const Option = styled.div`
+const Option = styled.div<{ isSelected: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  row-gap: 12px;
+  row-gap: 8px;
   width: 95px;
   height: 95px;
   flex-shrink: 0;
   border-radius: 10px;
-  border: 0.5px solid #b2b2b2;
-  background: #fff;
+  border: ${(props) =>
+    props.isSelected ? "0.5px solid #B6B6B6" : "0.5px solid #b2b2b2"};
+  background: ${(props) => (props.isSelected ? "#E8EBFA" : "#FFF")};
   box-shadow: 0px 0.5px 2px 0px rgba(97, 100, 107, 0.5) inset;
 `;
 
